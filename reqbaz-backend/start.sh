dcd5ef3b71e2a8d8cbfb8a72c23d3a2ecc321fca
Philipp Hossner <philipp.hossner@posteo.de>
2019-02-07 19:31:54 +0100

    Add reqbaz containers

---
+++ reqbaz-backend/start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+while ! mysqladmin ping -hmysql --silent; do
+    echo "Waiting for mysql..."
+    sleep 1
+done
+ant migrate-db
+mysql -uroot -preqbaz -hmysql reqbaz < ./etc/migrations/add_reqbaz_demo_data_full.sql
+
+java -cp "lib/*" i5.las2peer.tools.L2pNodeLauncher -p 9011 uploadStartupDirectory startService\(\'de.rwth.dbis.acis.bazaar.service.BazaarService@0.7.2\',\'Passphrase\'\) startWebConnector
