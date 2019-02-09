4a106de4cc11122ce6fdbe783451810ccadac27b
Philipp Hossner <philipp.hossner@posteo.de>
2019-02-09 21:22:14 +0100

    Fix start commands

--- activity-tracker/start.sh
+++ activity-tracker/start.sh
@@ -7,4 +7,4 @@ while ! mysqladmin ping -hmysql --silent; do
 done
 ant migrate-db
 
-java -cp "lib/*" i5.las2peer.tools.L2pNodeLauncher -p 9011 -b reqbaz-backend:9011 uploadStartupDirectory startService\(\'de.rwth.dbis.acis.activitytracker.service.ActivityTrackerService@0.5.0\',\'Passphrase\'\) startWebConnector
+java -cp "lib/*:service/*" i5.las2peer.tools.L2pNodeLauncher -p 9011 -b reqbaz-backend:9011 uploadStartupDirectory startService\(\'de.rwth.dbis.acis.activitytracker.service.ActivityTrackerService@0.5.0\',\'Passphrase\'\) startWebConnector
