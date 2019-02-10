f46964b2cad3a4ff8864bc039d5e67b1a1d331b3
Philipp Hossner <philipp.hossner@posteo.de>
2019-02-10 14:52:35 +0100

    Add MobSOS data processing

--- activity-tracker/start.sh
+++ activity-tracker/start.sh
@@ -7,4 +7,9 @@ while ! mysqladmin ping -hmysql --silent; do
 done
 ant migrate-db
 
-java -cp "lib/*:service/*" i5.las2peer.tools.L2pNodeLauncher -p 9011 -b reqbaz-backend:9011 uploadStartupDirectory startService\(\'de.rwth.dbis.acis.activitytracker.service.ActivityTrackerService@0.5.0\',\'Passphrase\'\) startWebConnector
+while ! </dev/tcp/mobsos-data-processing/9011; do
+    echo "Waiting for las2peer bootstrap..."
+    sleep 1
+done
+
+java -cp "lib/*:service/*" i5.las2peer.tools.L2pNodeLauncher -p 9011 -b mobsos-data-processing:9011 --observer uploadStartupDirectory startService\(\'de.rwth.dbis.acis.activitytracker.service.ActivityTrackerService@0.5.0\',\'Passphrase\'\) startWebConnector
--- reqbaz-backend/start.sh
+++ reqbaz-backend/start.sh
@@ -8,4 +8,9 @@ done
 ant migrate-db
 mysql -uroot -preqbaz -hmysql reqbaz < ./etc/migrations/add_reqbaz_demo_data_full.sql
 
-java -cp "lib/*:service/*" i5.las2peer.tools.L2pNodeLauncher -p 9011 uploadStartupDirectory startService\(\'de.rwth.dbis.acis.bazaar.service.BazaarService@0.7.2\',\'Passphrase\'\) startWebConnector
+while ! </dev/tcp/mobsos-data-processing/9011; do
+    echo "Waiting for las2peer bootstrap..."
+    sleep 1
+done
+
+java -cp "lib/*:service/*" i5.las2peer.tools.L2pNodeLauncher -p 9011 -b mobsos-data-processing:9011 uploadStartupDirectory --observer startService\(\'de.rwth.dbis.acis.bazaar.service.BazaarService@0.7.2\',\'Passphrase\'\) startWebConnector
