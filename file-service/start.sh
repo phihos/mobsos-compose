cf56fdbe2bd9540f7b26d6eea40b346305b82706
Philipp Hossner <philipp.hossner@posteo.de>
2019-02-20 19:23:23 +0100

    Update submodules and remove obsolete Dockerfiles

--- file-service/start.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-while ! </dev/tcp/mobsos-data-processing/9011; do
-    echo "Waiting for las2peer bootstrap..."
-    sleep 1
-done
-
-java -cp "lib/*" i5.las2peer.tools.L2pNodeLauncher -p 9011 -b mobsos-data-processing:9011 --observer -s ./service/ startService\(\'i5.las2peer.services.fileService.FileService@2.2.5\',\'Passphrase\'\) startWebConnector
